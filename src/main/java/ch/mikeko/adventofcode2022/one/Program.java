2a8044b3cddad01a98345d385e0808fe2ba61ccd
Mike Koch <mkoch227@gmail.com>
2022-12-02 23:16:22 -0500

    Day one

---
+++ src/main/java/ch/mikeko/adventofcode2022/one/Program.java
@@ -0,0 +1,41 @@
+package ch.mikeko.adventofcode2022.one;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Program {
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        var list = new ArrayList<Integer>();
+
+        URL resource = Program.class.getResource("/ch/mikeko/adventofcode2022/one/input.txt");
+        try (Stream<String> lines = Files.lines(Path.of(Objects.requireNonNull(resource).toURI()), Charset.defaultCharset())) {
+            var collectedLines = lines.collect(Collectors.toList());
+
+            var runningTotal = 0;
+            for (var line : collectedLines) {
+                if ("".equals(line)) {
+                    list.add(runningTotal);
+                    runningTotal = 0;
+                    continue;
+                }
+
+                runningTotal += Integer.parseInt(line);
+            }
+        }
+
+        list.sort((o1, o2) -> o2 - o1);
+
+        System.out.println(list.get(0));
+    }
+}
