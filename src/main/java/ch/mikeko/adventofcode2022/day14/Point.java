c2086b3c0e45f296e9a8c729dd488d85351dcfad
Mike Koch <mkoch227@gmail.com>
2022-12-17 14:40:15 -0500

    Day 14 part one

---
+++ src/main/java/ch/mikeko/adventofcode2022/day14/Point.java
@@ -0,0 +1,56 @@
+package ch.mikeko.adventofcode2022.day14;
+
+import java.util.Objects;
+
+public class Point {
+    private int x;
+    private int y;
+    private String symbol = "#";
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return x == point.x && y == point.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Point{%s,%s,%s}", x, y, symbol);
+    }
+}
