c90c90b9ee16ae60ef4a6bd0eed35c2f4381ad99
Mike Koch <mkoch227@gmail.com>
2022-12-16 19:44:01 -0500

    Day 10 part 2

--- src/main/java/ch/mikeko/adventofcode2022/day10/Cpu.java
+++ src/main/java/ch/mikeko/adventofcode2022/day10/Cpu.java
@@ -1,11 +1,13 @@
 package ch.mikeko.adventofcode2022.day10;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Cpu {
-    private int cycleCount = 1;
-    private int xValue = 1;
+    private int cycleCount = 0;
+    private int spritePosition = 1;
+    private int crtPosition = 0;
     private final Map<Integer, Integer> signalStrengths = new HashMap<>();
 
     public void noop() {
@@ -14,19 +16,38 @@ public class Cpu {
 
     public void addx(int value) {
         increaseCycleCount();
-        xValue += value;
         increaseCycleCount();
+        spritePosition += value;
     }
 
     private void increaseCycleCount() {
         cycleCount++;
 
         if (cycleCount == 20 || (cycleCount - 20) % 40 == 0) {
-            signalStrengths.put(cycleCount, cycleCount * xValue);
+            signalStrengths.put(cycleCount, cycleCount * spritePosition);
         }
+
+        drawCrt();
     }
 
     public Map<Integer, Integer> getSignalStrengths() {
         return signalStrengths;
+    }
+
+    private void drawCrt() {
+        var validPixels = Arrays.asList(spritePosition - 1, spritePosition, spritePosition + 1);
+
+        if (validPixels.contains(crtPosition)) {
+            System.out.print("#");
+        } else {
+            System.out.print(".");
+        }
+        crtPosition++;
+
+
+        if (cycleCount % 40 == 0) {
+            System.out.println();
+            crtPosition = 0;
+        }
     }
 }
